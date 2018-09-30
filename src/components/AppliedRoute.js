3e288b4a383046fd676f0ae5c744e60b13ee393e
Cameron Moten <33769422+cameronmoten@users.noreply.github.com>
2018-09-29 17:07:36 -0700

    TypeScript Conversion
    Type Script -- Working v1

--- src/components/AppliedRoute.js
+++ src/components/AppliedRoute.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+//Getting a warning of slowness when you use Lambda, should maybe change this later.
 export default ({ component: C, props: cProps, ...rest }) =>
   <Route {...rest} render={props => <C {...props} {...cProps} />} />;
